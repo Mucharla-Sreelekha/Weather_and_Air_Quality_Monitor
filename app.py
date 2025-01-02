e829a4955269a9d7539151051fc4ab7192748cc6
Mucharla Sreelekha <sreelekhaoff@gmail.com>
2025-01-02 17:49:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -878,4 +878,4 @@ def create_admin():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensure tables are created
-    app.run(debug=True)
+    app.run(debug=False)
