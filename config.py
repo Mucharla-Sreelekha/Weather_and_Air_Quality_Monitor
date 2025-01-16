25170785182c0ed8e9c65e999c7e56edf3fc5fa3
Mucharla Sreelekha <sreelekhaoff@gmail.com>
2025-01-16 22:55:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://if0_38119649:vRFGIgSGlGg8xH4@sql101.infinityfree.com/if0_38119649_infosys'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://if0_38119649:vRFGIgSGlGg8xH4@sql101.infinityfree.com:3306/if0_38119649_infosys'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     GOOGLE_OAUTH_CLIENT_ID = '541441980761-tmhlqs9pt59a43vicec0lc9o7j09rtsl.apps.googleusercontent.com'
