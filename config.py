4df4ab4799ece93778a8ec53cd5fc650f1e93853
Mucharla Sreelekha <sreelekhaoff@gmail.com>
2025-01-16 19:08:26 +0530

    adding new DB

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12755282:qGA79JdVp8@sql12.freesqldatabase.com/sql12755282'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://sql12758000:sX7ayFH9R8@sql12.freesqldatabase.com/sql12758000'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     GOOGLE_OAUTH_CLIENT_ID = '541441980761-tmhlqs9pt59a43vicec0lc9o7j09rtsl.apps.googleusercontent.com'
